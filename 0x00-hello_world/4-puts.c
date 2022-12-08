a2ab65ba5db37e081ab1b397f1a0e39ac1ffba94
Jhaylino <mennia38@gmail.com>
2022-12-08 12:46:56 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-
 /**
- *  main - Entry Point 
- *  Return: Always 0 (Success)
+ *  main - prints a given phrade
+ *  Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
