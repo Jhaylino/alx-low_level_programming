0d7ad2d410f767e7b60ac2a6b88e3d748d39fbac
Jhaylino <mennia38@gmail.com>
2022-12-12 17:12:33 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 0; n <= 9; n++)
-        {
-                putchar(n + '0');
-                if (n < 9)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
