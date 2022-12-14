9603cd5d84b70e9810ed9a2e634047172e7793f2
Jhaylino <mennia38@gmail.com>
2022-12-13 18:44:16 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	for (a = 0; b <= 23; a++)
+	for (a = 0; a <= 23; a++)
 	{
 		for (b = 0; b <= 59; b++)
 		{
-			_putchar (a / 1- + '0');
+			_putchar (a / 10 + '0');
 			_putchar (a % 10 + '0');
 			_putchar (':');
 			_putchar (b / 10 + '0');
