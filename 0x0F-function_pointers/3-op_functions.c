e7093f6665c0cca221a6c7e2b23c9a8f89dfd820
Jhaylino <mennia38@gmail.com>
2023-01-18 16:43:21 -0800

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,39 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
 /**
- * op_add -Adds rwo numbers 
- * @a: a number
- * @b: a number
- * Return: Sum of a and b
- */
+ * op_add - adds @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-/**
- * op_sub - Calculates then difference of 2 numbers
- * @a: a number
- * @b: a number
- *
- * Return: Difference of a and b
- */
 
+/**
+ * op_sub - gets the difference of @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: difference of @a and @b
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-/**
- * op_mul - calcautes the multiplication
- * @a: a number
- * @b: a number
- *
- * Return: result of mul
- */
 
+/**
+ * op_mul - multiplies @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: product of @a and @b
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
@@ -41,38 +42,28 @@ int op_mul(int a, int b)
 
 
 /**
- * op_div - calcuates divison
- * @a: a number
- * @b: a number
+ * op_div - divides @a by @b
  *
- * Return: result of div
- */
-
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: results of the division @a and @b
+*/
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
-/**
- * op_mod - calculates mod
- * @a: a number
- * @b: a number
- * Return: result of mod
- */
 
+/**
+ * op_mod - gets the reminder of the division between @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: the remainder
+*/
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
